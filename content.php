103d75dfa00002196bbd22ee317c6f8bb45bce6c
fangjun <fjun99@gmail.com>
2014-12-10 21:22:20 +0800

    jiathis

--- content.php
+++ content.php
@@ -64,15 +64,6 @@
             <a href="http://www.jiathis.com/share?uid=1993513" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank"></a>
             <a class="jiathis_counter_style"></a>
         </div>
-        <script type="text/javascript" >
-            var jiathis_config={
-                data_track_clickback:true,
-                summary:"",
-                shortUrl:false,
-                hideMore:false
-            }
-        </script>
-        <script type="text/javascript" src="http://v3.jiathis.com/code/jia.js?uid=1993513" charset="utf-8"></script>
         <!-- JiaThis Button END -->
 
 
